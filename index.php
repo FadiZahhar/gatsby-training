e936550f5f4898c675856607a0ab1d1fd5844ec0
Fadi Zahhar <79freelancer@gmail.com>
2021-11-25 13:45:29 +0200

    adding the first course

---
+++ index.php
@@ -0,0 +1,98 @@
+<h1>Gatsby first training<h1>
+
+<h2>Tutorial and Project</h2>
+
+
+<h3>Intro</h3>
+<iframe src="https://player.vimeo.com/video/648654069" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>video blur fix</h3>
+<iframe src="https://player.vimeo.com/video/648653862" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Course review</h3>
+<iframe src="https://player.vimeo.com/video/648653962" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Gatsby CLI</h3>
+<iframe src="https://player.vimeo.com/video/648654205" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Setup new project</h3>
+<iframe src="https://player.vimeo.com/video/648654536" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+
+<h3>Visual Studio Code</h3>
+<iframe src="https://player.vimeo.com/video/648653973" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Project Structure</h3>
+<iframe src="https://player.vimeo.com/video/648654652" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>First Page</h3>
+<iframe src="https://player.vimeo.com/video/648655192" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Error Page </h3>
+<iframe src="https://player.vimeo.com/video/648655453" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Nested Pages</h3>
+<iframe src="https://player.vimeo.com/video/648655524" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Links</h3>
+<iframe src="https://player.vimeo.com/video/648655627" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Navbar</h3>
+<iframe src="https://player.vimeo.com/video/648655814" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Layout Component</h3>
+<iframe src="https://player.vimeo.com/video/648655984" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>CSS Intro</h3>
+<iframe src="https://player.vimeo.com/video/648656197" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Inline CSS</h3>
+<iframe src="https://player.vimeo.com/video/648656252" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Global CSS</h3>
+<iframe src="https://player.vimeo.com/video/648656322" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>CSS Naming issues</h3>
+<iframe src="https://player.vimeo.com/video/648656436" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>CSS Modules</h3>
+<iframe src="https://player.vimeo.com/video/648656535" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Styled Component</h3>
+<iframe src="https://player.vimeo.com/video/648656739" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>House Cleaning</h3>
+<iframe src="https://player.vimeo.com/video/648657114" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Styles</h3>
+<iframe src="https://player.vimeo.com/video/648657396" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Footer</h3>
+<iframe src="https://player.vimeo.com/video/648657565" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Error Page</h3>
+<iframe src="https://player.vimeo.com/video/648657676" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Contact Page</h3>
+<iframe src="https://player.vimeo.com/video/648657834" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Assets and Icons</h3>
+<iframe src="https://player.vimeo.com/video/648657956" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Navbar Setup</h3>
+<iframe src="https://player.vimeo.com/video/648658262" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Navbar functinality</h3>
+<iframe src="https://player.vimeo.com/video/648658648" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Image info</h3>
+<iframe src="https://player.vimeo.com/video/648658751" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>Sandbox setup</h3>
+<iframe src="https://player.vimeo.com/video/648658977" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h3>install plugin mp4</h3>
+<iframe src="https://player.vimeo.com/video/648659091" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+<h1>Gatsby second coruse</h1>
+<h2>Gatsby Developer guide</h2>
